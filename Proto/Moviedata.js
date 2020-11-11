d76933c4f1fbcffb7989ae78ad97b3f03e7847bb
Jonas <j.klepach@hotmail.de>
2020-11-11 21:55:53 +0100

    Init proto files

---
+++ Proto/Moviedata.js
@@ -0,0 +1,28 @@
+var data=[
+{
+    "title":"Alien1",
+    "provider":"hard disk",
+    "watched_date":"2020-10-03",
+    "rating":[
+        {
+        "user":"jonas",
+        "points":4
+        }
+    ]
+} ,
+{
+    "title":"Alien2",
+    "provider":"hard disk",
+    "watched_date":"2020-10-03",
+    "rating":[
+        {
+        "user":"jonas",
+        "points":3
+        },
+        {
+            "user":"oli",
+            "points":4
+            }
+    ]
+}   
+]
